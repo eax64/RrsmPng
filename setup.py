fdb61f2650bee9408f0f1fb3f4aabaa566c49371
Kevin Soules <inceax64@gmail.com>
2015-04-22 00:05:44 +0200

    Remove python version restriction in setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,6 @@
 import sys
 from distutils.core import setup
 
-# if sys.version_info < (3, 0):
-#     sys.stdout.write("Sorry, requires Python 3.x.\n")
-#     sys.exit(1)
-
 setup(name='rrsmpng',
       version='0.1',
       author="Kevin `eax64` Soules",
